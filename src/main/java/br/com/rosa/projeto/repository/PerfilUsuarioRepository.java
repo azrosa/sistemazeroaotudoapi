39e957ccc2e7e60d73b0e639acadf79f444c7315
Alan Rosa <azevedo.rosa@hotmail.com>
2023-11-29 20:36:54 -0300

    ManyToOne(perfil_usuario)

---
+++ src/main/java/br/com/rosa/projeto/repository/PerfilUsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.rosa.projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.rosa.projeto.entity.PerfilUsuarioEntity;
+
+public interface PerfilUsuarioRepository extends JpaRepository<PerfilUsuarioEntity, Long>{
+
+}
